bf10dab38460fcb3154be94cce7e6d3bd0ff15f6
Giovanni <s331574@studenti.polito.it>
2024-03-23 16:36:21 +0100

    Creazione esercizio2

---
+++ LAB01ES02/target/debug/Esercizio2.d
@@ -0,0 +1,1 @@
+C:\Users\giova\Desktop\progetti\rust\LAB01ES02\target\debug\Esercizio2.exe: C:\Users\giova\Desktop\progetti\rust\LAB01ES02\src\main.rs
---
+++ LAB01ES02/target/debug/deps/Esercizio2.d
@@ -0,0 +1,5 @@
+C:\Users\giova\Desktop\progetti\rust\LAB01ES02\Esercizio2\target\debug\deps\Esercizio2.exe: src\main.rs
+
+C:\Users\giova\Desktop\progetti\rust\LAB01ES02\Esercizio2\target\debug\deps\Esercizio2.d: src\main.rs
+
+src\main.rs:
