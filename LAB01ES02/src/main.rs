10028518b5ce0a3b1a0ecde3378ced5d46c64dec
Giovanni <s331574@studenti.polito.it>
2024-03-23 20:49:07 +0100

    continuo esercizio2

--- LAB01ES02/src/main.rs
+++ LAB01ES02/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
+enum Error{
+    Simple(SystemTime);
+    Complex(SystemTime, String); 
+}
+impl Error{
+
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+    //read_to_string
+    let s1=fs::read_to_string("../test.txt");
+    // s1 é una tupla, se file non esiste mi da errore
+    println!("{:?}",s1);
+
+    //read
+    let s2=fs::read("../test.txt");
+    println!("{:?}",s2);
+
 }
